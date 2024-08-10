08ea0f5190e49f95bc452d307d78b7c29c6eb917
v.khremuchkov <mineexmail@gmail.com>
2024-08-10 14:19:50 -0400

    escrow init

---
+++ rs/escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("4vjv23D4TWp1mLeGiwSFbbWJ9uNQdekXx276eof6DZhX");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
